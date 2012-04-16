d6919c362e06605b7178718e72b3e69454300cf7
Soki Sakurai <sakurai@juno.phys.s.u-tokyo.ac.jp>
2012-04-16 12:18:58 +0900

    Revised XMLParserX.hpp

--- XMLParserX.hpp
+++ XMLParserX.hpp
@@ -27,12 +27,54 @@
 
 
 struct XMLHandlerX: public xercesc::DefaultHandler {
-	XMLHandlerX(XMLNode** top);
+	XMLHandlerX(XMLNode** top){
+        topnode = top;
+        XMLNode* dummytopnode = new XMLNode("DUMMYTOPNODE");
+        nodestack.push(dummytopnode);
+    }
 
 	void startElement(const XMLCh* const uri, const XMLCh* const localname, const XMLCh* const qname,
-			const xercesc::Attributes& attrs);
-	void characters(const XMLCh* const chars, const XMLSize_t length);
-	void endElement(const XMLCh* const uri, const XMLCh* const localname, const XMLCh* const qname);
+                      const xercesc::Attributes& attrs){
+        char* name = xercesc::XMLString::transcode(localname);
+        XMLNode* nexttopnode = new XMLNode(std::string(name));
+        XMLNode* currenttopnode = nodestack.top();
+        currenttopnode->addchild(std::string(name),nexttopnode);
+        nodestack.push(nexttopnode);
+        
+        int max_attributes = attrs.getLength();
+        for(int i = 0;i<max_attributes;i++){
+            char* locname = xercesc::XMLString::transcode(attrs.getLocalName(i));
+            char* value = xercesc::XMLString::transcode(attrs.getValue(i));
+            nexttopnode->addAttribute(std::string(locname),std::string(value));
+            xercesc::XMLString::release(&locname);
+            xercesc::XMLString::release(&value);
+        }
+        buf = "";
+        xercesc::XMLString::release(&name);
+    }
+
+	void characters(const XMLCh* const chars, const XMLSize_t length){
+        char* name = xercesc::XMLString::transcode(chars);
+        std::string val = std::string(name);
+        buf = buf+val;
+        xercesc::XMLString::release(&name);
+    }
+    
+	void endElement(const XMLCh* const uri, const XMLCh* const localname, const XMLCh* const qname){
+        XMLNode* currenttopnode = nodestack.top();
+        buf.erase(0,buf.find_first_not_of(" \t\n",0));
+        buf = buf.substr(0,buf.find_last_not_of(" \t\n",-1)+1);
+        currenttopnode->setValue(buf);
+        buf = "";
+        if(nodestack.size()==2){
+            *topnode = nodestack.top();
+            nodestack.pop();
+            XMLNode* dummytopnode = nodestack.top();
+            delete dummytopnode;
+        }
+        nodestack.pop();
+    }
+
 
 private:
 	std::stack<XMLNode*> nodestack;
@@ -41,61 +83,17 @@ private:
 };
 
 
-XMLHandlerX::XMLHandlerX(XMLNode** top){
-	topnode = top;
-	XMLNode* dummytopnode = new XMLNode("DUMMYTOPNODE");
-	nodestack.push(dummytopnode);
-}
-
-void XMLHandlerX::startElement(const XMLCh* const uri, const XMLCh* const localname, const XMLCh* const qname,
-		const xercesc::Attributes& attrs){
-	char* name = xercesc::XMLString::transcode(localname);
-	XMLNode* nexttopnode = new XMLNode(std::string(name));
-	XMLNode* currenttopnode = nodestack.top();
-	currenttopnode->addchild(std::string(name),nexttopnode);
-	nodestack.push(nexttopnode);
-
-	int max_attributes = attrs.getLength();
-	for(int i = 0;i<max_attributes;i++){
-		char* locname = xercesc::XMLString::transcode(attrs.getLocalName(i));
-		char* value = xercesc::XMLString::transcode(attrs.getValue(i));
-		nexttopnode->addAttribute(std::string(locname),std::string(value));
-		xercesc::XMLString::release(&locname);
-		xercesc::XMLString::release(&value);
-	}
-	buf = "";
-	xercesc::XMLString::release(&name);
-}
-
-void XMLHandlerX::characters(const XMLCh* const chars, const XMLSize_t length){
-	char* name = xercesc::XMLString::transcode(chars);
-	std::string val = std::string(name);
-	buf = buf+val;
-	xercesc::XMLString::release(&name);
-}
-
-void XMLHandlerX::endElement(const XMLCh* const uri, const XMLCh* const localname, const XMLCh* const qname){
-	XMLNode* currenttopnode = nodestack.top();
-	buf.erase(0,buf.find_first_not_of(" \t\n",0));
-	buf = buf.substr(0,buf.find_last_not_of(" \t\n",-1)+1);
-	currenttopnode->setValue(buf);
-	buf = "";
-	if(nodestack.size()==2){
-		*topnode = nodestack.top();
-		nodestack.pop();
-		XMLNode* dummytopnode = nodestack.top();
-		delete dummytopnode;
-	}
-	nodestack.pop();
-}
-
-
-
-
 class XMLParserX{
 public:
-	XMLParserX(std::string fname="");
-	virtual ~XMLParserX();
+	XMLParserX(std::string fname=""){
+        // TODO Auto-generated constructor stub
+        xercesc::XMLPlatformUtils::Initialize();
+        parser = xercesc::XMLReaderFactory::createXMLReader();
+        filename = fname;
+        handler = NULL;
+    }
+	virtual ~XMLParserX(){
+    }
 
 private:
 	xercesc::SAX2XMLReader* parser;
@@ -103,9 +101,24 @@ private:
 	std::string filename;
 
 public:
-	void parse();
-	void setHandler(XMLHandlerX* phandler);
-	void setFileName(std::string fname);
+	void parse(){
+        if(filename==""){
+            throw XMLParserException(XMLParserException::NoSuchFile);
+        }
+        else if(handler==NULL){
+            throw XMLParserException(XMLParserException::NoHandler);
+        }
+        parser->parse(filename.c_str());
+    }
+    
+	void setHandler(XMLHandlerX* phandler){
+        handler = phandler;
+        parser->setContentHandler(phandler);
+    }
+    
+	void setFileName(std::string fname){
+        filename = fname;
+    }
 
 public:
 	class XMLParserException{
@@ -119,36 +132,5 @@ public:
 	};
 };
 
-
-XMLParserX::XMLParserX(std::string fname){
-	// TODO Auto-generated constructor stub
-	xercesc::XMLPlatformUtils::Initialize();
-	parser = xercesc::XMLReaderFactory::createXMLReader();
-	filename = fname;
-	handler = NULL;
-}
-
-XMLParserX::~XMLParserX(){
-	// TODO Auto-generated destructor stub
-}
-
-void XMLParserX::parse(){
-	if(filename==""){
-		throw XMLParserException(XMLParserException::NoSuchFile);
-	}
-	else if(handler==NULL){
-		throw XMLParserException(XMLParserException::NoHandler);
-	}
-	parser->parse(filename.c_str());
-}
-
-void XMLParserX::setHandler(XMLHandlerX* phandler){
-	handler = phandler;
-	parser->setContentHandler(phandler);
-}
-
-void XMLParserX::setFileName(std::string fname){
-	filename = fname;
-}
 
 #endif /* XMLPARSERX_H_ */
